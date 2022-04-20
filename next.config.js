213ce8e953b5f59e0059f2f0dd3ba21ace588146
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-20 10:35:44 -0400

    chore: add images domains to next config gile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['www.oascomputer.com', 'www.bios.do'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
