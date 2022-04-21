53106878354e9ec2a32204759939b360c74fdc7a
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-20 22:38:05 -0400

    feat: shows laptops with styles

--- components/LaptopCard.tsx
+++ components/LaptopCard.tsx
@@ -1,12 +1,25 @@
-import Image from 'next/image';
+import { Badge, Box } from '@chakra-ui/react';
 import { IProduct } from '../interfaces/storeDataProps';
 
 export const LaptopCard = ({ name, imgUrl, price, store }: IProduct) => {
   return (
-    <div>
-      <p>{name}</p>
-      <Image src={imgUrl} alt={name} width={120} height={120} />
-      <p>{price}</p>
-    </div>
+    <Box>
+      <Box display='flex' alignItems='center' justifyContent='center'>
+        <img src={imgUrl} alt={name} width={220} height={220} />
+      </Box>
+
+      <Box p='6'>
+        <Box fontWeight='semibold' as='h3' lineHeight='tight' isTruncated>
+          {name}
+        </Box>
+
+        <Badge borderRadius='full' px='2' colorScheme='teal'>
+          {price}
+        </Badge>
+        <Box mt='1' fontWeight='bold' as='h4' lineHeight='tight' isTruncated>
+          {store}
+        </Box>
+      </Box>
+    </Box>
   );
 };
